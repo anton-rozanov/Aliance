be142517cfe5a0f0adf1bc929821c89b14db0421
anton-rozanov <taverman777@mail.ru>
2022-11-15 06:09:38 +0300

    add: switching menu(js)

--- js/main.js
+++ js/main.js
@@ -2,17 +2,47 @@ const navbar = document.querySelector(".navbar");
 const logo = document.querySelector(".logo-svg use");
 const mMenuToggle = document.querySelector(".mobile-menu-toggle");
 const menu = document.querySelector(".mobile-menu");
-window.addEventListener('scroll',()=>{
-  if(this.scrollY > 1){
-    navbar.classList.add("navbar-light");
-    logo.href.baseVal =   "img/sprite.svg#logo";
+
+const lightModeOn = (event) =>{
+  navbar.classList.add("navbar-light");
+  logo.href.baseVal =   "img/sprite.svg#logo";
+};
+const lightModeOff = (event) =>{
+  navbar.classList.remove("navbar-light");
+  logo.href.baseVal =   "img/sprite.svg#logo-light";
+};
+
+const openMenu = (event) => { // функция открывания меню
+  menu.classList.add('is-open'); // вешает класс is-open
+  mMenuToggle.classList.add('close-menu');
+  document.body.style.overflow="hidden"; // запрещаем прокрутку сайта под меню
+  lightModeOn();
+};
+const closeMenu = (event) => { // функция закрвания меню
+  menu.classList.remove('is-open'); // убирает класс is-open
+  mMenuToggle.classList.remove('close-menu');
+  document.body.style.overflow=""; // разрешает прокрутку сайта под меню
+  lightModeOff();
+};
+
+window.addEventListener('scroll', () => {
+  //ДО
+  /* if(this.scrollY > 1){
+    lightModeOn();
   } else{
-    navbar.classList.remove("navbar-light");
-    logo.href.baseVal =   "img/sprite.svg#logo-light";
-  }
+    lightModeOff();
+  } */
+  //ПОСЛЕ
+  this.scrollY > 1 ? lightModeOn() : lightModeOff(); // 1.условие  2. ? - if , 3. : - else
 });
 mMenuToggle.addEventListener('click',(event) =>{
-event.preventDefault();
-  menu.classList.toggle('is-open');
-
+  event.preventDefault();
+  // ДО
+  /*if (menu.classList.contains('is-open')) { // если меню открыто ... 
+    closeMenu(); // закрыть меню
+  } else { // иначе ...
+    openMenu(); // открыть меню
+  }*/
+  // ПОСЛЕ
+  menu.classList.contains('is-open') ? closeMenu() : openMenu(); // 1.условие  2. ? - if , 3. : - else
 });
